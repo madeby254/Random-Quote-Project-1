c914bf208fd27178129a7e60665df5c8c72327ce
Mohamed Nuh <tap-mnuh@Mohameds-Air.home>
2020-02-27 20:09:43 -0800

    Array Quotes

--- js/script.js
+++ js/script.js
@@ -10,6 +10,41 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {
+    quote: "I don't fight for the money. I fight for my legacy. I fight for history. I fight for my people.",
+    source: 'Khabib Nurmagomedov',
+    year: "2018",
+    genre: "Sports"
+  },
+  {
+    quote: "Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.",
+    source: 'Barack Obama,',
+    genre: " Politics"
+  },
+  {
+    quote: 'Kindness in words creates confidence. Kindness in thinking creates profoundness. Kindness in giving creates love',
+    source: 'Lao Tzu',
+    genre: " Philosophy"
+  },
+  {
+    quote: 'Let us always meet each other with smile, for the smile is the beginning of love.',
+    source: 'Mother Teresa',
+    genre: " Religion"
+  },
+  {
+    quote: 'Education is the most powerful weapon which you can use to change the world.',
+    source: 'Nelson Mandela',
+    year: "2013",
+    genre: " Education"
+  },
+  {
+    quote: 'Hi i am Dave',
+    source: 'Dave McFarland',
+    year: "1800",
+    genre: " Treehouse"
+  },
+];
 
 
 
