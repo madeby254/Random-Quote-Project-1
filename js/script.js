3ed30934268f644b95cfb9ad812d55c505e0871a
Mohamed Nuh <tap-mnuh@Mohameds-Air.home>
2020-02-28 16:41:41 -0800

    Hello

--- js/script.js
+++ js/script.js
@@ -17,34 +17,38 @@ const quotes = [
     quote: "I don't fight for the money. I fight for my legacy. I fight for history. I fight for my people.",
     source: 'Khabib Nurmagomedov',
     year: "2018",
-    genre: "Sports"
+    genre: "Sports",
+    citation:'ufc.com'
   },
   {
     quote: "Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.",
     source: 'Barack Obama,',
-    genre: " Politics"
+    genre: " Politics",
+    citation: 'Wikipedia'
   },
   {
     quote: 'Kindness in words creates confidence. Kindness in thinking creates profoundness. Kindness in giving creates love',
     source: 'Lao Tzu',
-    genre: " Philosophy"
+    genre: " Philosophy",
   },
   {
     quote: 'Let us always meet each other with smile, for the smile is the beginning of love.',
     source: 'Mother Teresa',
-    genre: " Religion"
+    genre: " Religion",
   },
   {
     quote: 'Education is the most powerful weapon which you can use to change the world.',
     source: 'Nelson Mandela',
     year: "2013",
-    genre: " Education"
+    genre: " Education",
+ 
   },
   {
     quote: 'Hi am Dave',
     source: 'Dave McFarland',
     year: "1800",
-    genre: " Treehouse"
+    genre: " Treehouse",
+    citation:'Treehouse Techdegree Intro'
   },
 ];
 
@@ -75,10 +79,14 @@ const  printQuote = () => {
 	};
 	if ( retrieveQuote.genre ) {
 		printing += "<cite class='genre'>" + retrieveQuote.genre + "</cite>"
-	};
+  }; 
+  if (retrieveQuote.citation) {
+    printing += "<cite class='citation'>" + retrieveQuote.citation + "</cite>"
+  }
 	printing  += "</p"
 	document.getElementById( 'quote-box' ).innerHTML = printing;
 };
+
 
 
 /***
